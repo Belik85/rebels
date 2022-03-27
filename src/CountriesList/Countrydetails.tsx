159350071482ba27d27fa363511bb5d44b8c9d1c
Belik85 <beletskyidmytro@gmail.com>
2022-03-28 00:27:18 +0200

    get details from api

--- src/CountriesList/Countrydetails.tsx
+++ src/CountriesList/Countrydetails.tsx
@@ -1,12 +1,13 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import './Countrydetails.css'
 import {useParams} from "react-router-dom";
 import {countryApi} from "../api/country";
 
-const Countrydetails = () => {
+const Countrydetails = (props:any) => {
     const {country} = useParams()
 
     useEffect(() => {
+
         countryApi.get(`name/${country?.toLowerCase()}`).then(({data}) => console.log(data[0]))
     })
     return <div >
