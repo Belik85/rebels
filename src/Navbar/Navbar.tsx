ff75cb050bfc00605f147fab866b6141b6a56077
Belik85 <beletskyidmytro@gmail.com>
2022-03-27 21:23:33 +0200

    creating structure

---
+++ src/Navbar/Navbar.tsx
@@ -0,0 +1,11 @@
+import './Navbar.css'
+
+const Navbar = () => {
+    return <div className="mode">
+        <span className="textleft">Where in the world?</span>
+        <span className="textright" onClick={() => {
+        }}>Dark mode</span>
+    </div>
+}
+
+export default Navbar;
